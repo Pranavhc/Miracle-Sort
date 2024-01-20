32271253994433a197361e54314258c4dacd4e10
Pranav Chandane <pranavhchandane@gmail.com>
2024-01-20 18:19:56 +0530

    commitment is crucial.

---
+++ miracle_sort.py
@@ -0,0 +1,8 @@
+def miracle_sort(arr: []) -> arr[]:
+  """ 'And, when you want something, all the universe conspires in helping
+      you to achieve it' - Melchizedek, the King of Salem (from 'The Alchemist').
+  """
+  # if you truly want this array to be sorted, it will be.
+  miracle = arr # In theory, the miracle should happen here
+  
+  return miracle
