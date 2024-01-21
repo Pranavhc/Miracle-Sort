f545d1326662f41bd0e3ed61d1cf8d83be54c08d
Pranav Chandane <pranavhchandane@gmail.com>
2024-01-21 14:01:16 +0530

    miracle miracle miracle

---
+++ miracle_sort.rs
@@ -0,0 +1,9 @@
+/// According to the king of Salem, Melchizedek, If you truly want this array to be sorted, depending upon the strength
+/// of your desire, it will be sorted. All the universe will conspire to defeat the chaos and establish order. 
+
+/// Note - without "Desire Activation", the algorithm will fail. 
+
+fn miracle_sort<T: PartialOrd>(arr: Vec<T>) -> Vec<T> {    
+  let mut miracle: Vec<T> = arr; // Miracle will happen. You have to believe in it.
+  miracle /// The reward of your desire shall be the order you sought, proven stronger over chaos.
+}
